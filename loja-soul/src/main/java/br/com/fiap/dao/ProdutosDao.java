52b49a3f20c715fa2ab60e3686e8fb5b5187cd46
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-07 18:10:04 -0300

    feat: implementação do metodo de atualização da tabela, servindo para editar a tabela caso ocorra alguma modificação

--- loja-soul/src/main/java/br/com/fiap/dao/ProdutosDao.java
+++ loja-soul/src/main/java/br/com/fiap/dao/ProdutosDao.java
@@ -7,7 +7,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
 public class ProdutosDao {
 
     public Connection minhaConexao;
@@ -17,23 +16,38 @@ public class ProdutosDao {
     }
 
     public String inserir(Produtos produto) throws SQLException {
+        // Tabela atualizada aqui
         PreparedStatement stmt = minhaConexao.prepareStatement("Insert Into TB_SOUL_PRODUTOS values (?,?,?,?,?)");
         stmt.setInt(1, produto.getId());
         stmt.setString(2, produto.getNome());
         stmt.setInt(3, produto.getQuantidade());
         stmt.setString(4, produto.getTipo());
         stmt.setDouble(5, produto.getPontos());
+
         stmt.execute();
         stmt.close();
         return "Produto Cadastrado com Sucesso!!!";
     }
-
+    public String atualizar(Produtos produto) throws SQLException {
+        PreparedStatement stmt = minhaConexao.prepareStatement(
+                "Update TB_SOUL_PRODUTOS set NOME =?, QUANTIDADE =?, TIPO =?, PONTOS =? where ID =?");
+        stmt.setString(1, produto.getNome());
+        stmt.setInt(2, produto.getQuantidade());
+        stmt.setString(3, produto.getTipo());
+        stmt.setDouble(4, produto.getPontos());
+        stmt.setInt(5, produto.getId());
+        stmt.executeUpdate();
+        stmt.close();
+        return "Produto Atualizado com Sucesso!!!";
+    }
     public String deletar(int id) throws SQLException {
+        // Tabela atualizada aqui
         PreparedStatement stmt = minhaConexao.prepareStatement("Delete From TB_SOUL_PRODUTOS where ID =?");
         stmt.setInt(1, id);
 
         stmt.execute();
         stmt.close();
-        return "Produto Deletado com Sucesso!!!";
-    }
+        return "Produto Deletado com Sucesso!!!";}
+
+
 }
