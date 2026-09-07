1a533d634820a001eaf6cd11b7f9ba401b0a2453
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-07 18:49:12 -0300

    feat: conexão com o banco de dados oracle feita

--- loja-soul/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
+++ loja-soul/src/main/java/br/com/fiap/conexoes/ConexaoFactory.java
@@ -1,4 +1,17 @@
 package br.com.fiap.conexoes;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoFactory {
+
+    public Connection conexao() throws ClassNotFoundException, SQLException {
+
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl",
+                "RM569064", "040608");
+    }
 }
