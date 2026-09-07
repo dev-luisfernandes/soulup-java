6068ae788759c6de905442e485ca03d7d94c8e1f
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-07 18:37:31 -0300

    feat: id do produto adicionado na classe Produtos.java

--- loja-soul/src/main/java/br/com/fiap/entities/Produtos.java
+++ loja-soul/src/main/java/br/com/fiap/entities/Produtos.java
@@ -2,19 +2,29 @@ package br.com.fiap.entities;
 
 public class Produtos {
 
+    private int id;
     private String nome;
     private int quantidade;
     private String tipo;
-    private double preco;
+    private double pontos;
 
     public Produtos() {
     }
 
-    public Produtos(String nome, int quantidade, String tipo, double preco) {
+    public Produtos(int id, String nome, int quantidade, String tipo, double pontos) {
+        this.id = id;
         this.nome = nome;
         this.quantidade = quantidade;
         this.tipo = tipo;
-        this.preco = preco;
+        this.pontos = pontos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -41,20 +51,22 @@ public class Produtos {
         this.tipo = tipo;
     }
 
-    public double getPreco() {
-        return preco;
+    public double getPontos() {
+        return pontos;
     }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
     }
 
     @Override
     public String toString() {
-        return "\n\n--Produtos--" +
-                "\nNome = " + nome +
-                "\nQuantidade = " + quantidade +
-                "\nTipo = " + tipo +
-                "\nPreço = " + preco;
+        return "Produtos{" +
+                "\n\nid: " + id +
+                "\nnome: " + nome + '\'' +
+                "\nquantidade:" + quantidade +
+                "\ntipo: " + tipo + '\'' +
+                "\npontos: " + pontos +
+                '}';
     }
 }
