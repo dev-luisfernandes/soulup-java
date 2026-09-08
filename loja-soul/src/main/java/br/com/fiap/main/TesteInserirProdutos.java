35265351a5b555f5c45d50e41623e5df5b104778
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-08 17:24:01 -0300

    feat: metodo throws adicionado para caso de o erro, definição do que vai ser adicionado no banco de dados, pop up para mostrar que o produto foi cadastrado (por um import la do dao no inserir )

--- loja-soul/src/main/java/br/com/fiap/main/TesteInserirProdutos.java
+++ loja-soul/src/main/java/br/com/fiap/main/TesteInserirProdutos.java
@@ -1,5 +1,11 @@
 package br.com.fiap.main;
+
+//import do dao pq ele pega a função criada lá e adiciona apenas a funcionalidade aqui basicamente o que o usuario faz
+import br.com.fiap.dao.ProdutosDao;
+import br.com.fiap.entities.Produtos;
+
 import javax.swing.JOptionPane;
+import java.sql.SQLException;
 
 
 public class TesteInserirProdutos {
@@ -11,4 +17,22 @@ public class TesteInserirProdutos {
     static double real(String j) {
         return Double.parseDouble(JOptionPane.showInputDialog(j)); }
 
+    //funcionalidade ligada no nosso banco de dados
+    // O throws avisa o java q se der algum erro com o Oracle (SQLException),
+    // ele passa o erro para quem executou, o main e o main passa onde esta executando, a maquina e se der erro o programa para
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ProdutosDao dao = new ProdutosDao();
+        Produtos objProduto = new Produtos();
+
+        // o responsavel define tudo manualmente para adicionar na nossa tabela no sql
+        objProduto.setId(inteiro("ID do Novo Produto:"));
+        objProduto.setNome(texto("Nome do Produto:"));
+        objProduto.setQuantidade(inteiro("Quantidade em Estoque:"));
+        objProduto.setTipo(texto("Tipo:"));
+        objProduto.setPontos(real("Custo em Pontos:"));
+
+        JOptionPane.showMessageDialog(
+                null,
+                dao.inserir(objProduto));
+    }
 }
