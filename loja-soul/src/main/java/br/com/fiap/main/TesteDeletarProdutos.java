e1d362c44986a7023e476f73134f341d0c37f116
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-08 17:35:57 -0300

    feat: classe TesteDeletarProdutos.java criada e metodo para deletar a partir do ID do produto adicionado, conectado diretamente pelo metodo de deletar criado na classe dao

---
+++ loja-soul/src/main/java/br/com/fiap/main/TesteDeletarProdutos.java
@@ -0,0 +1,19 @@
+package br.com.fiap.main;
+
+import br.com.fiap.dao.ProdutosDao;
+import br.com.fiap.entities.Produtos;
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class TesteDeletarProdutos {
+
+    static int inteiro(String j) { return Integer.parseInt(JOptionPane.showInputDialog(j)); }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Produtos objProdutos = new Produtos();
+        ProdutosDao dao = new ProdutosDao();
+
+        objProdutos.setId(inteiro("Informe o ID que será deletado"));
+        System.out.println(dao.deletar(objProdutos.getId()));
+    }
+}
