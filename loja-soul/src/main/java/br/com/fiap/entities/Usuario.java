ca09d73c5026b0f9deaede8ff9f2e361759fe31e
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 09:05:54 -0300

    feat: metodos getter $ seter, constructor e to string adicionados

--- loja-soul/src/main/java/br/com/fiap/entities/Usuario.java
+++ loja-soul/src/main/java/br/com/fiap/entities/Usuario.java
@@ -5,6 +5,57 @@ public class Usuario {
     private int CPF;
     private String nome;
     private double pontos;
-    private
+    private int CEP;
 
+    public Usuario() {
+    }
+
+    public Usuario(int CPF, String nome, double pontos, int CEP) {
+        this.CPF = CPF;
+        this.nome = nome;
+        this.pontos = pontos;
+        this.CEP = CEP;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
+
+    public int getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(int CEP) {
+        this.CEP = CEP;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "CPF=" + CPF +
+                ", nome='" + nome + '\'' +
+                ", pontos=" + pontos +
+                ", CEP=" + CEP +
+                '}';
+    }
 }
