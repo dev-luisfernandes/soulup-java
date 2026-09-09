d8e9a58f466ec60f07668bde653b157c38af257f
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 14:14:58 -0300

    feat: metodo de deletar usuario do banco de dados com base no seu CPF

---
+++ loja-soul/src/main/java/br/com/fiap/main/usuario/TesteDeletarUsuario.java
@@ -0,0 +1,21 @@
+package br.com.fiap.main.usuario;
+
+import br.com.fiap.dao.UsuarioDao;
+
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+public class TesteDeletarUsuario {
+
+    static String texto(String j) {
+        return JOptionPane.showInputDialog(j);
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        UsuarioDao dao = new UsuarioDao();
+        String cpf = texto("Digite o CPF do usuário que será DELETADO:");
+
+        JOptionPane.showMessageDialog(null, dao.deletar(cpf));
+    }
+}
