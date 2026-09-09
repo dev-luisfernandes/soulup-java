d2f7cf893dcf1672b4748ffa15c114725455ce16
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 19:15:29 -0300

    feat: id retirado de o usuario definir pois agora virou automatico

--- loja-soul/src/main/java/br/com/fiap/main/produto/TesteInserirProdutos.java
+++ loja-soul/src/main/java/br/com/fiap/main/produto/TesteInserirProdutos.java
@@ -24,7 +24,6 @@ public class TesteInserirProdutos {
         ProdutosDao dao = new ProdutosDao();
         Produtos objProduto = new Produtos();
 
-        objProduto.setId(inteiro("defina o id do produto:"));
         objProduto.setNome(texto("Nome do Produto:"));
         objProduto.setQuantidade(inteiro("Quantidade em Estoque:"));
         objProduto.setTipo(texto("Tipo:"));
