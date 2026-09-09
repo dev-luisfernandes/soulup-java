b1fed8b4c5703ffff3f20a578b886f84abc69212
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 11:25:05 -0300

    feat: ajuste para compatibilidade com o banco de dados

--- loja-soul/src/main/java/br/com/fiap/main/produto/TesteInserirProdutos.java
+++ loja-soul/src/main/java/br/com/fiap/main/produto/TesteInserirProdutos.java
@@ -1,5 +1,4 @@
 package br.com.fiap.main.produto;
-
 //import do dao pq ele pega a função criada lá e adiciona apenas a funcionalidade aqui basicamente o que o usuario faz
 import br.com.fiap.dao.ProdutosDao;
 import br.com.fiap.entities.Produtos;
@@ -7,16 +6,17 @@ import br.com.fiap.entities.Produtos;
 import javax.swing.JOptionPane;
 import java.sql.SQLException;
 
-
 public class TesteInserirProdutos {
 
-    static String texto(String j) {
-        return JOptionPane.showInputDialog(j); }
-    static int inteiro(String j) {
-        return Integer.parseInt(JOptionPane.showInputDialog(j)); }
-    static double real(String j) {
-        return Double.parseDouble(JOptionPane.showInputDialog(j)); }
-
+    static String texto(String j){
+        return JOptionPane.showInputDialog(j);
+    }
+    static int inteiro(String j){
+        return Integer.parseInt(JOptionPane.showInputDialog(j));
+    }
+    static double real(String j){
+        return Double.parseDouble(JOptionPane.showInputDialog(j));
+    }
     //funcionalidade ligada no nosso banco de dados
     // O throws avisa o java q se der algum erro com o Oracle (SQLException),
     // ele passa o erro para quem executou, o main e o main passa onde esta executando, a maquina e se der erro o programa para
@@ -24,8 +24,6 @@ public class TesteInserirProdutos {
         ProdutosDao dao = new ProdutosDao();
         Produtos objProduto = new Produtos();
 
-        // o responsavel define tudo manualmente para adicionar na nossa tabela no sql
-        objProduto.setId(inteiro("ID do Novo Produto:"));
         objProduto.setNome(texto("Nome do Produto:"));
         objProduto.setQuantidade(inteiro("Quantidade em Estoque:"));
         objProduto.setTipo(texto("Tipo:"));
