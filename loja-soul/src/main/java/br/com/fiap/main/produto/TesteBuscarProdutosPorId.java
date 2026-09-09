8f0c31bd2418ba593bbc73561d9981cdec6c5569
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 10:32:54 -0300

    feat: metodo de buscar o produto por um id adicionado

---
+++ loja-soul/src/main/java/br/com/fiap/main/produto/TesteBuscarProdutosPorId.java
@@ -0,0 +1,27 @@
+package br.com.fiap.main.produto;
+
+import br.com.fiap.dao.ProdutosDao;
+import br.com.fiap.entities.Produtos;
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class TesteBuscarProdutosPorId {
+
+    static int inteiro(String j) { return Integer.parseInt(JOptionPane.showInputDialog(j)); }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Produtos objProdutos = new Produtos();
+        ProdutosDao dao = new ProdutosDao();
+
+        objProdutos.setId(inteiro("Informe o ID para busca"));
+        objProdutos = dao.buscarPorId(objProdutos.getId());
+
+        //aqui se o produto existir mesmo ele vai retornar
+        if (objProdutos != null) {
+            System.out.println(objProdutos.toString());
+            //aqui, padrão... ele só aparece que n foi encontrado mesmo
+        } else {
+            System.out.println("Não encontrado no banco de dados!");
+        }
+    }
+}
