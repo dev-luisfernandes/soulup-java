50e1836d3930137d13f391649ca775f8ad5a4373
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 14:08:41 -0300

    feat: Definição de id adicionado em ProdutosDao

--- loja-soul/src/main/java/br/com/fiap/dao/ProdutosDao.java
+++ loja-soul/src/main/java/br/com/fiap/dao/ProdutosDao.java
@@ -18,9 +18,9 @@ public class ProdutosDao {
     }
 
     public String inserir(Produtos produto) throws SQLException {
-        // Tabela atualizada aqui
-        //PreparedStatement é o que leva o sql do java para o oracle, ou seja, ele é o uber das informações
-        PreparedStatement stmt = minhaConexao.prepareStatement("Insert Into TB_SOUL_PRODUTOS values (?,?,?,?,?)");
+        PreparedStatement stmt = minhaConexao.prepareStatement(
+                "INSERT INTO TB_SOUL_PRODUTOS (ID, NOME, QUANTIDADE, TIPO, PONTOS) VALUES (?, ?, ?, ?, ?)");
+
         stmt.setInt(1, produto.getId());
         stmt.setString(2, produto.getNome());
         stmt.setInt(3, produto.getQuantidade());
@@ -31,36 +31,37 @@ public class ProdutosDao {
         stmt.close();
         return "Produto Cadastrado com Sucesso!!!";
     }
+
     public String atualizar(Produtos produto) throws SQLException {
         PreparedStatement stmt = minhaConexao.prepareStatement(
-                "Update TB_SOUL_PRODUTOS set NOME =?, QUANTIDADE =?, TIPO =?, PONTOS =? where ID =?");
+                "Update TB_SOUL_PRODUTOS set NOME =?, QUANTIDADE =?, TIPO =?, PONTOS =? where ID = ?");
         stmt.setString(1, produto.getNome());
         stmt.setInt(2, produto.getQuantidade());
         stmt.setString(3, produto.getTipo());
         stmt.setDouble(4, produto.getPontos());
         stmt.setInt(5, produto.getId());
+
         stmt.executeUpdate();
         stmt.close();
         return "Produto Atualizado com Sucesso!!!";
     }
+
     public String deletar(int id) throws SQLException {
-        // ele deleta da tabela que a gente criou, mas só o produto com o ID que bate, o id digitado na hora de excluir
-        PreparedStatement stmt = minhaConexao.prepareStatement("Delete From TB_SOUL_PRODUTOS where ID =?");
+        PreparedStatement stmt = minhaConexao.prepareStatement("Delete From TB_SOUL_PRODUTOS where ID = ?");
         stmt.setInt(1, id);
 
         stmt.execute();
         stmt.close();
-        return "Produto Deletado com Sucesso!!!";}
+        return "Produto Deletado com Sucesso!!!";
+    }
 
     public Produtos buscarPorId(int id) throws SQLException {
         Produtos produto = null;
-        // aq basicamente diz pra selecionar todas as colunas da tabela TB... porem só as informações do ID selecionado
-        PreparedStatement stmt = minhaConexao.prepareStatement("Select * from TB_SOUL_PRODUTOS where ID=?");
+        PreparedStatement stmt = minhaConexao.prepareStatement("Select * from TB_SOUL_PRODUTOS where ID = ?");
         stmt.setInt(1, id);
-        //resulset basicamente a resposta da oracle que fica armazenada no java,
-        //rs é so a referencia pra n ficar digitando toda hora ResultSet
+
         ResultSet rs = stmt.executeQuery();
-        //esse next serve para andar linha por linha dessa repsosta para extrair as informações
+
         if (rs.next()) {
             produto = new Produtos();
             produto.setId(rs.getInt(1));
@@ -73,8 +74,8 @@ public class ProdutosDao {
         stmt.close();
         return produto;
     }
-    public ArrayList<Produtos> selecionar() throws SQLException {
 
+    public ArrayList<Produtos> selecionar() throws SQLException {
         ArrayList<Produtos> listaProdutos = new ArrayList<>();
 
         PreparedStatement stmt = minhaConexao.prepareStatement("Select * from TB_SOUL_PRODUTOS");
