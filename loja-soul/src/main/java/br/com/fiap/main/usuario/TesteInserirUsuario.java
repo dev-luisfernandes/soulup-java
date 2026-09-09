44b0dbc75e28a75e2c22e8aca444c1f9c50acf82
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 14:14:13 -0300

    feat: inserir email, telefone e data de nascimento atribuito ao usuario no banco de dados

---
+++ loja-soul/src/main/java/br/com/fiap/main/usuario/TesteInserirUsuario.java
@@ -0,0 +1,43 @@
+package br.com.fiap.main.usuario;
+
+import br.com.fiap.dao.UsuarioDao;
+import br.com.fiap.entities.Usuario;
+
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+public class TesteInserirUsuario {
+
+    static String texto(String j){
+        return JOptionPane.showInputDialog(j);
+    }
+    static int inteiro(String j){
+        return Integer.parseInt(JOptionPane.showInputDialog(j));
+    }
+    static double real(String j){
+        return Double.parseDouble(JOptionPane.showInputDialog(j));
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Usuario objUsuario = new Usuario();
+        UsuarioDao dao = new UsuarioDao();
+
+        /*private String CPF;
+    private String nome;
+    private double pontos;
+    private String telefone;
+    private String email;
+    private String dataNascimento;*/
+
+        objUsuario.setCPF(texto("Digite o CPF nesse formato: (xxxxxxxxxx):"));
+        objUsuario.setNome(texto("Nome do Usuário:"));
+        objUsuario.setPontos(real("Pontuação Inicial (ex: 0):"));
+        objUsuario.setTelefone(texto("Digite o numero ex:' (xx)xxxxxxxxx ':"));
+        objUsuario.setEmail(texto("digite o email:"));
+        objUsuario.setDataNascimento(texto("digite a data de nascimento no formato DD/MM/AA "));
+
+        // Envia para o banco e mostra a mensagem de sucesso no meio da tela
+        JOptionPane.showMessageDialog(
+                null, dao.inserir(objUsuario));
+    }
+}
