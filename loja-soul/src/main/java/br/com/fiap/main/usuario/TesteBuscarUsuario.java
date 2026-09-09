df0b30f44fcac6b24e4f8ba43d6d510b71161044
dev-luisfernandes <luisfelipefernandes.corp@gmail.com>
2026-09-09 14:06:15 -0300

    feat: email, telefone e data de nascimento adicionados as informações do usuario

---
+++ loja-soul/src/main/java/br/com/fiap/main/usuario/TesteBuscarUsuario.java
@@ -0,0 +1,46 @@
+package br.com.fiap.main.usuario;
+
+import br.com.fiap.dao.UsuarioDao;
+import br.com.fiap.entities.Usuario;
+
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+public class TesteBuscarUsuario {
+
+    static String texto(String j){
+        return JOptionPane.showInputDialog(j);
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        UsuarioDao dao = new UsuarioDao();
+        String cpfBusca = texto("Digite o CPF do usuário para ver a ficha completa:");
+
+        Usuario usuarioEncontrado = dao.buscarPorCpf(cpfBusca);
+
+        /*private String CPF;
+    private String nome;
+    private double pontos;
+    private String telefone;
+    private String email;
+    private String dataNascimento;*/
+        if (usuarioEncontrado != null) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "O USUÁRIO FOI ENCONTRADO!\n\n" +
+                            "Nome: " + usuarioEncontrado.getNome() + "\n" +
+                            "CPF: " + usuarioEncontrado.getCPF() + "\n" +
+                            "Telefone: " + usuarioEncontrado.getTelefone() + "\n" +
+                            "Email: " + usuarioEncontrado.getEmail() + "\n" +
+                            "Data de Nascimento: " + usuarioEncontrado.getDataNascimento() + "n" +
+                            "Saldo Ecológico: " + usuarioEncontrado.getPontos() + " pontos");
+
+        } else {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Ops! Nenhum usuário encontrado com esse CPF.");
+
+        }
+    }
+}
